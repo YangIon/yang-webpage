bb0020950829295f5b4ad78e62bf57916520dd97
Richard Chen <yyangc88@gmail.com>
2019-06-09 22:06:36 -0400

    Navbar dropdown edit; added assets; new homepage content

--- scripts/sticky_navbar.js
+++ scripts/sticky_navbar.js
@@ -1,26 +1,19 @@
+var lastScrollPos = window.pageYOffset;
 var navbar = document.getElementById("navbar");
 
-        var scrollPos = 0;
+window.onscroll = function() {
+    var curScrollPos = window.pageYOffset;
+    if (lastScrollPos > curScrollPos) {
+        navbar.style.top = "0";
+        navbar.style.backgroundColor = "#000000";
+    } else {
+        navbar.style.top = "-" + 300 + "px";
+    }
+    if (curScrollPos == 0) {
+        navbar.style.backgroundColor = "";
+    }
+    lastScrollPos = curScrollPos;
+}
 
-        window.addEventListener("scroll", function () {
-            var st = window.pageYOffset || document.documentElement.scrollTop;
 
-            if (st > scrollPos) {
-                navbar.classList.add("title-out");
-                setTimeout(function () {
-                    navbar.classList.remove("sticky");
-                    navbar.style.backgroundColor = "";
-                }, 600);
-            } else {
-                navbar.classList.remove("title-out");
-                navbar.classList.add("sticky");
-                navbar.classList.add("shadow-3")
-                if (st == 0) {
-                    navbar.style.backgroundColor = "";
-                    navbar.classList.remove("shadow-3");
-                } else {
-                    navbar.style.backgroundColor = "#000000";
-                }
-            }
-            scrollPos = st <= 0 ? 0 : st;
-        }, false);
+
