bb721e9ed5446bf53ff7bf6ecd626aad19ab9bd5
Richard Chen <yyangc88@gmail.com>
2019-06-11 13:49:39 -0400

    Additional layout; new node structure

--- scripts/sticky_navbar.js
+++ scripts/sticky_navbar.js
@@ -1,5 +1,11 @@
 var lastScrollPos = window.pageYOffset;
 var navbar = document.getElementById("navbar");
+var dropdown = document.getElementsByClassName("dropdown");
+
+[...dropdown].forEach(ele => {
+    ele.style.backgroundColor = "transparent";
+    ele.style.top = "30px";
+})
 
 window.onscroll = function() {
     var curScrollPos = window.pageYOffset;
@@ -8,9 +14,17 @@ window.onscroll = function() {
         navbar.style.backgroundColor = "#000000";
     } else {
         navbar.style.top = "-" + 300 + "px";
+        [...dropdown].forEach(ele => {
+            ele.style.backgroundColor = "#000000";
+            ele.style.top = "45px";
+        });
     }
     if (curScrollPos == 0) {
-        navbar.style.backgroundColor = "";
+        navbar.style.backgroundColor = "transparent";
+        [...dropdown].forEach(ele => {
+            ele.style.backgroundColor = "transparent";
+            ele.style.top = "30px";
+        });
     }
     lastScrollPos = curScrollPos;
 }
